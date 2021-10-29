e8ecbfb8a72698053461f2d4849ac5bfb532b2e4
Nero103 <92405860+Nero103@users.noreply.github.com>
2021-10-29 12:22:45 -0400

    Update SQLQuery5_Nashville_housing.sql
    Corrected description in standardization section

--- SQLQuery5_Nashville_housing.sql
+++ SQLQuery5_Nashville_housing.sql
@@ -8,7 +8,7 @@ From
 
 --------------------------------------------------------------------------------
 
---Date Format Standaradization
+--Date Format Standardization
 
 Select 
 	ConvertedSaleDate,
