7f8e9309bb7d760138f151390616a3355c89fbb7
Nero103 <92405860+Nero103@users.noreply.github.com>
2021-10-29 12:26:34 -0400

    Removal
    Removed extra code I tried

--- SQLQuery5_Nashville_housing.sql
+++ SQLQuery5_Nashville_housing.sql
@@ -35,8 +35,8 @@ Where
 Order By
 	ParcelID
 
---Since the PropertyAddress has NULLS and the PArcelID can identify the address, if the ParcelID is present in the record, 
---the NULLs can be filled in with the PropertyAddress of table 2. A self_join will be used here
+--Since the PropertyAddress has NULLS and the ParcelID can identify the address, if the ParcelID is present in the record, 
+--the NULLs can be filled in with the PropertyAddress of table 2. A self-join will be used here
 
 Select 
 	nh1.ParcelID, nh1.PropertyAddress, nh2.ParcelID, nh2.PropertyAddress,
@@ -206,53 +206,3 @@ From
 
 Alter Table project_portfolio.dbo.nashville_housing
 DROP COLUMN OwnerAddress, PropertyAddress, SaleDate
-
------------------------------------------------------------------------------------------------
------------------------------------------------------------------------------------------------
-
---- Importing Data using OPENROWSET and BULK INSERT	
-
---  More advanced and looks cooler, but have to configure server appropriately to do correctly
---  Wanted to provide this in case you wanted to try it
-
-
---sp_configure 'show advanced options', 1;
---RECONFIGURE;
---GO
---sp_configure 'Ad Hoc Distributed Queries', 1;
---RECONFIGURE;
---GO
-
-
---USE PortfolioProject 
-
---GO 
-
---EXEC master.dbo.sp_MSset_oledb_prop N'Microsoft.ACE.OLEDB.12.0', N'AllowInProcess', 1 
-
---GO 
-
---EXEC master.dbo.sp_MSset_oledb_prop N'Microsoft.ACE.OLEDB.12.0', N'DynamicParameters', 1 
-
---GO 
-
-
----- Using BULK INSERT
-
---USE PortfolioProject;
---GO
---BULK INSERT nashvilleHousing FROM 'C:\Temp\SQL Server Management Studio\Nashville Housing Data for Data Cleaning Project.csv'
---   WITH (
---      FIELDTERMINATOR = ',',
---      ROWTERMINATOR = '\n'
---);
---GO
-
-
----- Using OPENROWSET
---USE PortfolioProject;
---GO
---SELECT * INTO nashvilleHousing
---FROM OPENROWSET('Microsoft.ACE.OLEDB.12.0',
---    'Excel 12.0; Database=C:\Users\alexf\OneDrive\Documents\SQL Server Management Studio\Nashville Housing Data for Data Cleaning Project.csv', [Sheet1$]);
---GO
